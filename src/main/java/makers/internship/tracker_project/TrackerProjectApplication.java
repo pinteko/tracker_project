aad6c6777d121afaf33978b8bcc360723422ec3f
Kirill Korsuk <korsukkirill@gmail.com>
2023-05-19 13:13:35 +0300

    Init commit

---
+++ src/main/java/makers/internship/tracker_project/TrackerProjectApplication.java
@@ -0,0 +1,13 @@
+package makers.internship.tracker_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrackerProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TrackerProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/makers/internship/tracker_project/TrackerProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package makers.internship.tracker_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrackerProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
